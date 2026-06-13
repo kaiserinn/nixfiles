7b8774ef9c00433cad567ec6028f5f7a2933ab6e
Azhar Rizqullah <azharizqullah90@gmail.com>
2026-06-13 18:37:48 +0800

    System rebuild at 2026-06-13 18:46

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
     nixpkgs.url = "nixpkgs/nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     kintsugi.url = "github:kaiserinn/kintsugi";
 
     stylix = {
-      url = "github:nix-community/stylix";
+      url = "github:nix-community/stylix/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
