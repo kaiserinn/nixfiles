fc40c0050bf2b72e31c6856d81c08515eebcb063
Azhar Rizqullah <azharizqullah90@gmail.com>
2026-09-22 00:52:12 +0800

    System rebuild at 2026-09-22 00:52

--- home-manager/modules/neovim/nvim/lua/commands.lua
+++ home-manager/modules/neovim/nvim/lua/commands.lua
@@ -4,25 +4,32 @@ local term = require("term")
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.hl.hl_op({ higroup = "Visual", timeout = 300 })
-	end,
-	group = highlight_group,
-	pattern = "*",
+    callback = function()
+        vim.hl.hl_op({ higroup = "Visual", timeout = 300 })
+    end,
+    group = highlight_group,
+    pattern = "*",
 })
 
 vim.api.nvim_create_user_command("Run", function(opts)
-	local cmd = opts.args
-	if cmd == "" then
-		return
-	end
-	term.run(cmd, { auto_resize = true })
+    local cmd = opts.args
+    if cmd == "" then
+        return
+    end
+    term.run(cmd, { auto_resize = true })
 end, { nargs = "*", complete = "shellcmd" })
 
 vim.api.nvim_create_user_command("Runx", function(opts)
-	local cmd = opts.args
-	if cmd == "" then
-		return
-	end
-	term.run(cmd, { auto_resize = true, close_on_exit = true })
+    local cmd = opts.args
+    if cmd == "" then
+        return
+    end
+    term.run(cmd, { auto_resize = true, close_on_exit = true })
 end, { nargs = "*", complete = "shellcmd" })
+
+vim.api.nvim_create_user_command("InsertDate", function()
+    local date = os.date("%d/%m/%Y") --[[@as string]]
+    vim.api.nvim_put({ date }, "c", true, true)
+end, {
+    desc = "Insert current date in dd/mm/yyyy format",
+})
